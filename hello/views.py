546bf17180e0fc7eeca1ea028f59506552c6d944
ns196414 <ns196414@ohio.edu>
2018-03-25 21:39:02 -0400

    Some updates will upload and download and file now.

--- hello/views.py
+++ hello/views.py
@@ -4,8 +4,31 @@ import requests
 from .models import Greeting
 from django.views.generic import TemplateView
 
-# class StaticView(TemplateView):
-#    template_name = "WeakOne.html"
+from django.shortcuts import render
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+# from myproject.myapp.models import Document
+# from myproject.myapp.forms import DocumentForm
+
+from hello.models import Document
+# from hello.forms import DocumentForm
+
+from django.conf import settings
+import os
+import subprocess
+import time
+
+def upload(request):
+    # Handle file upload
+    if request.method == 'POST':
+        xmlfile = request.FILES['xmlfile']
+        response = HttpResponse(xmlfile.read(), content_type="application/xhtml+xml")
+        response['Content-Disposition'] = 'attachment;filename=' + request.FILES['xmlfile'].name
+        return response
+
+    return render(request, 'index.html')
 
 def index(request):
     # return HttpResponse('Hello from Python!')
